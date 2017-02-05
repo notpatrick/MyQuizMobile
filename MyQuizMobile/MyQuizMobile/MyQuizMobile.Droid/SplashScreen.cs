73f234e453102f9de2f36c2eb25c03bcda9d3491
Patrick <notpatrick@users.noreply.github.com>
2017-02-05 13:56:49 +0100

    Add Android splash screen

---
+++ MyQuizMobile/MyQuizMobile/MyQuizMobile.Droid/SplashScreen.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Support.V7.App;
+
+namespace MyQuizMobile.Droid {
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity {
+        protected override void OnResume() {
+            base.OnResume();
+
+            var task = new Task(t => { StartActivity(new Intent(Application.Context, typeof(MainActivity))); },
+                                TaskScheduler.FromCurrentSynchronizationContext());
+            task.Start();
+        }
+    }
+}
